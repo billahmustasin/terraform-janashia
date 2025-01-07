243e64f4aa34ef0af38d11e0f6d428cd23f466d7
shuvo <shuvo@gmail.com>
2025-01-08 00:46:55 +0600

    variable keyboard input

--- main.tf
+++ main.tf
@@ -4,20 +4,36 @@ provider "aws" {
 
 resource "aws_vpc" "development-vpc" {
   cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name: "development"
+  }
+}
+
+variable "subnet_cidr_block" {
+  description = "subnet cidr block"
 }
 
 resource "aws_subnet" "dev-subnet-1" {
   vpc_id = aws_vpc.development-vpc.id
-  cidr_block = "10.0.10.0/24"
+  cidr_block = var.subnet_cidr_block
   availability_zone = "us-east-1a"
+    tags = {
+      Name: "subnet-1-dev"
+      vpc_env: "dev"
+  }
 }
 
 
-data "aws_vpc" "existing_vpc" {
-  default = true #means aws vpc default vpc will be used, go to management console 
-}
+# data "aws_vpc" "existing_vpc" {
+#   default = true #means aws vpc default vpc will be used, go to management console 
+# }
 
-resource "aws_subnet" "dev-subnet-2" {
-  vpc_id = data.aws_vpc.existing_vpc.id
-  cidr_block = "172.31.96.0/20"
-}
+# resource "aws_subnet" "dev-subnet-2" {
+#   vpc_id = data.aws_vpc.existing_vpc.id
+#   cidr_block = "172.31.96.0/20"
+#   availability_zone = "us-east-1a"
+#   tags = {
+#       Name: "subnet-2-default"
+#   }
+# }

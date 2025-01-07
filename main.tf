592beb7d2825c4d7325698de58367b01d7497c78
shuvo <shuvo@gmail.com>
2025-01-08 01:29:13 +0600

    variable tfvars input

--- main.tf
+++ main.tf
@@ -2,16 +2,20 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_vpc" "development-vpc" {
-  cidr_block = "10.0.0.0/16"
-
-  tags = {
-    Name: "development"
-  }
+variable "vpc_cidr_block" {
+  description = "vpc cidr block"
 }
 
 variable "subnet_cidr_block" {
   description = "subnet cidr block"
+}
+
+resource "aws_vpc" "development-vpc" {
+  cidr_block = var.vpc_cidr_block
+
+  tags = {
+    Name: "development"
+  }
 }
 
 resource "aws_subnet" "dev-subnet-1" {
